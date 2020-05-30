1cea03bab4f28d5670bdc740ed3be1de68a6e565
Agualuza <iagoagualuza@id.uff.br>
2020-05-30 12:30:56 -0300

    Adiciona o backend de todo fluxo da base de conhecimento

--- resources/views/read/index.blade.php
+++ resources/views/read/index.blade.php
@@ -9,38 +9,19 @@
                 <div class="card card-round card-read over-y-no-x">
                     <div class="card-body">
                         <div class="row">
+                            @foreach ($posts as $p)
                             <div class="card card-post card-round col-lg-3 col-md-4 col-sm-12 col-12 mx-1 ">
-                            <img class="card-img-top" src="../assets/img/post1.jpg">
+                            <img class="card-img-top" src="../assets/img/{{$p->img}}">
                             <div class="card-body">
-                                <p class="text-orange post-title">5 investimentos que rendem mais que a poupança</p>
-                                <a href="leitura/1" class="btn btn-primary">Ler</a>
+                                <p class="text-orange post-title">{{ $p->title }}</p>
+                                <a href="leitura/{{$p->id}}" class="btn btn-primary">Ler</a>
                             </div>
                             </div>
                             <br/>
                             <br/>
                             <br/>
                             <br/>
-                            <div class="card card-post card-round col-lg-3 col-md-4 col-sm-12 col-12 mx-1 ">
-                            <img class="card-img-top" src="../assets/img/post2.png">
-                            <div class="card-body">
-                                <p class="text-orange post-title">Como organizar as finanças?</p>
-                                <a href="leitura/2" class="btn btn-primary">Ler</a>
-                            </div>
-                            </div>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <div class="card card-post card-round col-lg-3 col-md-4 col-sm-12 col-12 mx-1 ">
-                            <img class="card-img-top" src="../assets/img/post3.png">
-                            <div class="card-body">
-                                <p class="text-orange post-title">Como aumentar o score de crédito?</p>
-                                <a href="leitura/3" class="btn btn-primary">Ler</a>
-                            </div>
-                            </div>
-
-                        
-                        
+                            @endforeach
                         </div>
                     </div>
                 </div>
--- resources/views/read/post.blade.php
+++ resources/views/read/post.blade.php
@@ -6,7 +6,7 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-12">
     <div class="card card-round col-12">
         <div>
-            <?php echo $post; ?>
+            <?php echo $post->content; ?>
         </div>
     </div>
 </div>
